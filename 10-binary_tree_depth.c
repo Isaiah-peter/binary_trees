c57fa2f28e69ee61334db191b4d0d1892230bfb5
Isaiah peter <peterisaiah4fun@gmail.com>
2022-09-21 13:58:27 +0100

    please

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -16,6 +16,15 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 		return (0);
 	}
 
-	const size_t depth_l = tree->parent ? 1 + binary_tree_depth(tree->parent) : 0;
+	size_t depth_l;
+
+	if (tree->parent != NULL)
+	{
+		depth_l = 1 + binary_tree_depth(tree->parent);
+	}
+	else
+	{
+		depth_l = 0;
+	}
 	return (depth_l);
 }
