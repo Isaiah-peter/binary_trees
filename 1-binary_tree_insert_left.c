57b6fafcb87e90c3fd81ee87e13a295d73bca16b
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-09-21 15:57:13 +0100

    is leaf

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,13 +3,14 @@
 #include <stdio.h>
 
 /**
- * *binary_tree_insert_left - inset to the left and side
+ * binary_tree_insert_left - A function that inserts a node as the left-child
+ *			of another node.
  *
- * @parent: root
- * @value: value
- * Return: binary_tree_node
+ * @parent: a pointer to the node to insert the left-child in
+ * @value: the value to store in the new node
+ * Return: return a pointer to the created node,
+ *	or NULL on failure or if parent is NULL.
  */
-
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *result = malloc(sizeof(binary_tree_t));
--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,27 +1,20 @@
 #include "binary_trees.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 
 /**
- * binary_tree_is_leaf - get the leaf
+ * binary_tree_is_leaf - checks if a node is a leaf
  *
- * @node: the node
- * Return: 0 or 1
+ * @node: pointer to the node to check
+ * Return: Return 1 if node is a leaf, otherwise 0.
  */
-
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
-	{
+		return (0)
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	else
 		return (0);
-	}
-
-	if (node->parent != NULL)
-	{
-		if (binary_tree_is_leaf(node->left) == 0 &&
-			binary_tree_is_leaf(node->right) == 0)
-		{
-			return (1);
-		}
-	}
-
-	return (0);
 }
