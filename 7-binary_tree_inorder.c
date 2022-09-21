1d973b573251649338ffbdda9514bb2c326a5c6f
Isaiah peter <peterisaiah4fun@gmail.com>
2022-09-21 13:20:55 +0100

    fix the failing test

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -10,6 +10,9 @@
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
+	if (func == NULL){
+        return;
+    }
 	if (tree != NULL)
 	{
 		binary_tree_inorder(tree->left, func);
