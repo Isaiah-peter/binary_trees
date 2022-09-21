c84fdb358ac4d65bb33872f45d756222a3992f02
Isaiah peter <peterisaiah4fun@gmail.com>
2022-09-21 09:54:29 +0100

    add space between if and ()

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,17 +7,16 @@
  * Return: 0 or 1
  */
 
-
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if(node == NULL)
+	if (node == NULL)
 	{
 		return (0);
 	}
 
-	if(node->parent != NULL)
+	if (node->parent != NULL)
 	{
-		if(binary_tree_is_leaf(node->left) == 0 && binary_tree_is_leaf(node->right) == 0 )
+		if (binary_tree_is_leaf(node->left) == 0 && binary_tree_is_leaf(node->right) == 0)
 		{
 			return (1);
 		}
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,7 +7,6 @@
  * Return: size
  */
 
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
