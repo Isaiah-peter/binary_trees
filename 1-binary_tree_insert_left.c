3eced706f06cc4cb2604cfccd8c6b4bcb177ee03
Isaiah peter <peterisaiah4fun@gmail.com>
2022-09-22 05:15:43 +0100

    task 12

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -42,5 +42,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left = result;
 	}
 
-	return (current);
+	return (result);
 }
---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - check if a tree tree is a pointer to
+ *  the root node of the tree to count the number of leaves 
+ * 
+ * @tree: a pointer to a tree structure
+ * Return: size_t of the number of leaves
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+    size_t right;
+    size_t left;
+
+    if (tree == NULL)
+    {
+        return (0);
+    }
+
+    if (tree->left == NULL && tree->right == NULL)
+    {
+        return (1);
+    }
+
+    right = tree->right ? 1 : 0;
+    left = tree->left ? 1 : 0;
+
+    return (left + right);
+}
