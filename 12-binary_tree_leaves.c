ea81c99bfa40f2090096411092ec938c643bb0ce
Isaiah peter <peterisaiah4fun@gmail.com>
2022-09-22 05:20:17 +0100

    fix betty

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -2,8 +2,8 @@
 
 /**
  * binary_tree_leaves - check if a tree tree is a pointer to
- *  the root node of the tree to count the number of leaves 
- * 
+ *  the root node of the tree to count the number of leaves
+ *
  * @tree: a pointer to a tree structure
  * Return: size_t of the number of leaves
  */
@@ -11,20 +11,20 @@
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
     size_t right;
-    size_t left;
+	size_t left;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
+	if (tree == NULL)
+	{
+		return (0);
+	}
 
-    if (tree->left == NULL && tree->right == NULL)
-    {
-        return (1);
-    }
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
 
-    right = tree->right ? 1 : 0;
-    left = tree->left ? 1 : 0;
+	right = tree->right ? 1 : 0;
+	left = tree->left ? 1 : 0;
 
-    return (left + right);
+	return (left + right);
 }
