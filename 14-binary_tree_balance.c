cd4d1b25f4fc5c4d1e50c3fd008267b940aa4dfa
Isaiah peter <peterisaiah4fun@gmail.com>
2022-09-22 22:08:29 +0100

    compile issue

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,11 +12,11 @@
  */
 size_t binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-
 	size_t height_l;
 	size_t height_r;
+
+	if (tree == NULL)
+		return (0);
 
 	height_l = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
 	height_r = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
