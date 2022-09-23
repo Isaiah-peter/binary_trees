94c8036afcddd9582111d0bc7d7bf2d7a6b6b747
Isaiah peter <peterisaiah4fun@gmail.com>
2022-09-23 05:21:31 +0100

    trying you first atempt again

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,24 +1,41 @@
 #include "binary_trees.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
-
-
 /**
- * binary_tree_balance - measures the balance factor of a binary tree
- *
- * @tree: pointer to the root node of the tree to measure the balance factor
- * Return: returns the balance factor of the tree, 0 if tree is NULL
+ * binary_tree_balance- count balance factor of tree
+ * @tree: the tree
+ * Return: int balanace or 0
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int height_l;
-	int height_r;
+	/* declarations */
+	int left, right;
 
-	if (tree == NULL)
+	/* check for tree */
+	if (!tree)
 		return (0);
+	/* if tree->left exists, call height func on that side, cast to int */
+	left = (tree->left) ? (int)binary_tree_height(tree->left) : -1;
+	/* if tree->right exists, call height func for it as welli, cast to int */
+	right = (tree->right) ? (int)binary_tree_height(tree->right) : -1;
+	/* get balance */
+	return (left - right);
+}
+/**
+ * binary_tree_height - get the height of tree
+ * @tree: the tree to measure
+ *Return: size_t height or 0
+ *
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	/* declarations */
+	size_t lh, rh;
 
-	height_l = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
-	height_r = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
-	return (height_l - height_r);
+	/* tree is null or a loner */
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+	/* measure both */
+	lh = binary_tree_height(tree->left);
+	rh = binary_tree_height(tree->right);
+	/* compare and return tallest */
+	return ((lh > rh ? lh : rh) + 1);
 }
