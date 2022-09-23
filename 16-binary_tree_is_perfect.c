15a8039dbd55119eae1086774b5bdd54cf8e5ad4
Isaiah peter <peterisaiah4fun@gmail.com>
2022-09-23 05:31:33 +0100

    more changes on 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -9,17 +9,17 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int levelL, levelR;
+
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
-	if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
-	{
-		return (0);
-	}
+	if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
+		return (1);
 
-	return (1);
+	return (0);
 }
 
 /**
