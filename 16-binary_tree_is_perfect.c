a200120547fb861bac470a544ba93e91d8e0f748
Isaiah peter <peterisaiah4fun@gmail.com>
2022-09-23 05:34:10 +0100

    do not use height to test

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -16,29 +16,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
-	if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
-		return (1);
+	levelL = binary_tree_is_perfect(tree->left);
+	levelR = binary_tree_is_perfect(tree->right);
 
-	return (0);
-}
-
-/**
- * binary_tree_height - get the height of tree
- * @tree: the tree to measure
- *Return: size_t height or 0
- *
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	
-	size_t lh, rh;
-
-	
-	if (!tree || (!tree->left && !tree->right))
+	if ((levelL == 0 || levelR == 0) && (levelL != levelR))
 		return (0);
-	
-	lh = binary_tree_height(tree->left);
-	rh = binary_tree_height(tree->right);
-	
-	return ((lh > rh ? lh : rh) + 1);
+	return (1);
 }
